0aeb3dd21961cb5837d18f2bfb58626ccb035c63
Devin Argenta <devin.argenta@buzzfeed.com>
2018-03-22 09:39:29 -0400

    update readme/add direclty to prototype

--- README.md
+++ README.md
@@ -1,9 +1,27 @@
 ## filtermap
 
-add this to ur array prototype even tho u shouldnt
+adds .filterMap  to ur array prototype even tho u shouldnt /shrug
 
 
 `[1,2,3,4].filterMap(callback, conditional, leading)`
 
 leading decides if the conditional should run before or after the callback
 true by default
+
+ex: 
+
+```
+import 'filtermap'
+
+let x = [1,2,3].filterMap((item) => item * 10, (item) => item >= 2)
+
+console.log(x)
+// [4, 6]
+
+let y = [1,2,3].filterMap((item) => item * 10, (item) => item >= 20, false)
+
+console.log(y)
+// [20, 30]
+
+
+```
--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-"use strict";
-
-exports.default = function(cb, fn) {
-  var leading = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : true;
-  var a = [],
-      arr = this;
-  leading ? arr.forEach(function (k) {
-    return fn(k) && a.push(cb(k));
-  }) : arr.forEach(function (k) {
-    var t = cb(k);
-    fn(t) && a.push(t);
-  });
-  return a;
-}
+(function() {
+  if (!Array.prototype.filterMap) {
+    Array.prototype.filterMap = function(cb, fn) {
+      var leading =
+        arguments.length > 2 && arguments[2] !== undefined
+          ? arguments[2]
+          : true;
+      var a = [],
+        arr = this;
+      leading
+        ? arr.forEach(function(k) {
+            return fn(k) && a.push(cb(k));
+          })
+        : arr.forEach(function(k) {
+            var t = cb(k);
+            fn(t) && a.push(t);
+          });
+      return a;
+    };
+  }
+})(this);
